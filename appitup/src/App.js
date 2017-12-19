731df1613a33cb3d9fa6fa9bb613cad0892735e4
cfissell50 <cfissell@hotmail.com>
2017-12-19 12:36:38 -0600

    wooo

--- appitup/src/App.js
+++ appitup/src/App.js
@@ -1,21 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './Pokeball.png';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const pokeStyle = {
+  textTransform: 'uppercase',
+  color: 'red',
 }
+class App extends Component {
+  state = {
+    characters: null
+
+  };
+
+  componentDidMount(){
+   const that = this;
+   fetch('https://pokeapi.co/api/v2/pokemon-form/?limit=20&offset=0')
+       .then((response) => response.json())
+       .then((responseJson) => {
+           that.setState({characters: responseJson.results})
+       })
+       .catch((error) => {
+           console.error(error);
+       });
+  }
+
+  constructor(props) {
+     super(props);
+     this.state = {open: false};
+   }
+
+   handleToggle = () => this.setState({open: !this.state.open});
+
+   handleClose = () => this.setState({open: false});
+  renderCharacters(){
+      const { characters } = this.state;
+      if(characters && characters.length > 0){
+          return characters.map((obj, key) =>{
+              return <p style={pokeStyle} key={key}>{obj.name}</p>
+          })
+      }
+  }
+
+
+   render() {
+     return (
+       <div className="App">
+         <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+           <h1 className="App-title">Pokemon</h1>
+         </header>
+           {this.renderCharacters()}
+       </div>
+     );
+   }
+ }
 
 export default App;
