c19ebc98bca23cabd8b14415ae313d8f3851e8a9
cfissell50 <cfissell@hotmail.com>
2017-12-19 18:24:39 -0600

    yuuuup

--- appitup/src/App 2.js
+++ appitup/src/App 2.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import logo from './Pokeball.png';
 import './App.css';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const style = {
+  margin: 12,
+};
 
 const styleText = {
   textTransform: 'uppercase',
@@ -37,23 +42,35 @@ class App extends Component {
       if(characters && characters.length > 0){
           return characters.map((obj, key) =>{
               return <a href={'https://en.wikipedia.org/wiki/' + obj.name}><p style={styleText} key={key}>{obj.name}</p></a>
-
           })
       }
   }
 
-   render() {
-     return (
-       <div className="App">
-         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-           <h1 className="App-title">Pokemon Info</h1>
-         </header>
-           {this.renderCharacters()}
-       </div>
+  const RaisedButtonExampleSimple = () => (
+    <div>
+      <RaisedButton label="Default" style={style} />
+      <RaisedButton label="Primary" primary={true} style={style} />
+      <RaisedButton label="Secondary" secondary={true} style={style} />
+      <RaisedButton label="Disabled" disabled={true} style={style} />
+      <br />
+      <br />
+      <RaisedButton label="Full width" fullWidth={true} />
+    </div>
+);
 
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Pokemon Info</h1>
+        </header>
+          {this.renderCharacters()}
+          {RaisedButtonExampleSimple()}
+      </div>
      );
    }
  }
 
+export default RaisedButtonExampleSimple;
 export default App;
